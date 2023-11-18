722b7bd166559fa16dd1495f01fbf1e351b51139
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2023-11-18 16:00:29 +0200

    Create install_essential_tools.bat

---
+++ choco/install_essential_tools.bat
@@ -0,0 +1,25 @@
+choco install git /NoAutoCrlf -y
+choco install \
+  vscode-insiders \ 
+  nvm \
+  helix \
+  dotnet-sdk  \
+  Microsoft-windows-terminal \
+  firacode \
+  azure-data-studio \ 
+  azure-cli \ 
+  azure-functions-core-tools \ 
+  visualstudio2022professional \ 
+  python \ 
+  powertoys \ 
+  drawio \ 
+  nuget.commandline \ 
+  terraform \ 
+  infracost \
+  postman \
+  7zip \
+  docker-desktop \
+  docker-compose \
+  gimp \
+  typora \
+  gh -y
